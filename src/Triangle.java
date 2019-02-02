4ab7cd77f281695a9da86edfc7b20f61d61076bc
Evan Lindsay <evanlindsay@live.com>
2019-02-02 03:40:26 -0800

    Arrange triangle vertices by y (and x if naturally flat)

--- src/Triangle.java
+++ src/Triangle.java
@@ -8,6 +8,7 @@ public class Triangle {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
+        arrangeByY();
     }
 
     public Vector4 getV1() {
@@ -26,5 +27,44 @@ public class Triangle {
         this.v1 = ctm.mult(this.v1);
         this.v2 = ctm.mult(this.v2);
         this.v3 = ctm.mult(this.v3);
+        arrangeByY();
+    }
+
+    public void arrangeByY() {
+        Vector4 tmp = null;
+
+        if (v2.y < v1.y) {
+            tmp = v1;
+            v1 = v2;
+            v2 = tmp;
+        }
+
+        if (v3.y < v2.y) {
+            tmp = v2;
+            v2 = v3;
+            v3 = tmp;
+        }
+
+        if (v2.y < v1.y) {
+            tmp = v1;
+            v1 = v2;
+            v2 = tmp;
+        }
+
+        if (v1.y == v2.y) {
+            // natural flat top
+            if (v2.x < v1.x) {
+                tmp = v1;
+                v1 = v2;
+                v2 = tmp;
+            }
+        } else if (v2.y == v3.y) {
+            // natural flat bottom
+            if (v3.x < v2.x) {
+                tmp = v2;
+                v2 = v3;
+                v3 = tmp;
+            }
+        }
     }
 }
