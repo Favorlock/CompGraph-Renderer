9ef1ee7a8df190994735df38559dcab241579dea
Evan Lindsay <evanlindsay@live.com>
2019-02-13 16:23:48 -0800

    2x AA

--- src/FrameBuffer.java
+++ src/FrameBuffer.java
@@ -54,6 +54,26 @@ public class FrameBuffer {
     }
 
     public void draw(WritableRaster raster, int x, int y) {
-        raster.setPixels(x, y, width, height, buffer);
+        int w = this.width / 2;
+        int h = this.height / 2;
+        double[] out = new double[w * h * 3];
+
+        for (int by = 0; by < this.height; by++) {
+            for (int bx = 0; bx < this.width; bx++) {
+                int ox = bx / 2;
+                int oy = by / 2;
+                int ri = oy * w * 3 + ox * 3;
+                out[ri] += buffer[by * this.width * 3 + bx * 3];
+                out[ri + 1] += buffer[by * this.width * 3 + bx * 3 + 1];
+                out[ri + 2] += buffer[by * this.width * 3 + bx * 3 + 2];
+                if (bx % 2 == 1 && by % 2 == 1) {
+                    out[ri] /= 4;
+                    out[ri + 1] /= 4;
+                    out[ri + 2] /= 4;
+                }
+            }
+        }
+
+        raster.setPixels(x, y, w, h, out);
     }
 }
